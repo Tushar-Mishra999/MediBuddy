a3f527b1b241687389f0083d08c78f55b0bbdf13
Tushar-Mishra999 <75738710+Tushar-Mishra999@users.noreply.github.com>
2023-02-06 10:45:36 +0530

    ui fixes for onboarding screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MeddiBuddy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -18,6 +19,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
--- app/lib/views/onboarding/onboarding_screen.dart
+++ app/lib/views/onboarding/onboarding_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -10,90 +8,102 @@ class OnboardingScreen extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(children: [
-          SizedBox(
-            height: size.height * 0.6,
-          ),
-          Center(
-              child: RichText(
-            text: const TextSpan(
-              children: [
-                TextSpan(
-                  text: "Medi",
+      body: SafeArea(
+        child: Center(
+          child: Column(children: [
+            SizedBox(
+              height: size.height * 0.07,
+            ),
+            Container(
+              margin: const EdgeInsets.all(2),
+              height: size.height * 0.4,
+              decoration: BoxDecoration(),
+              child: Image.asset(
+                "assets/images/doc3.jpeg",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Center(
+                child: RichText(
+              text: const TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Medi",
+                    style: TextStyle(
+                      color: Color(0xff8871E6),
+                      fontSize: 50,
+                      fontFamily: 'Gilroy',
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Buddy",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 50,
+                      fontFamily: 'Gilroy',
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                ],
+              ),
+            )),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Never run out of medicine,\n find nearby availability with one tap.',
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                style: TextStyle(
+                  color: Colors.grey.shade700,
+                  fontSize: 15,
+                  fontFamily: 'Gilroy',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            Container(
+              width: size.width * 0.75,
+              height: size.height * 0.07,
+              decoration: const BoxDecoration(
+                color: Color(0xff8871E6),
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+              child: const Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'Gilroy',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              width: size.width * 0.75,
+              height: size.height * 0.07,
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xff8871E6), width: 2),
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(Radius.circular(15)),
+              ),
+              child: const Center(
+                child: Text(
+                  'Register',
                   style: TextStyle(
                     color: Color(0xff8871E6),
-                    fontSize: 50,
+                    fontSize: 20,
                     fontFamily: 'Gilroy',
-                    fontWeight: FontWeight.w100,
                   ),
                 ),
-                TextSpan(
-                  text: "Buddy",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 50,
-                    fontFamily: 'Gilroy',
-                    fontWeight: FontWeight.w100,
-                  ),
-                ),
-              ],
-            ),
-          )),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Never run out of medicine, find nearby availability with one tap.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.grey.shade700,
-                fontSize: 15,
-                fontFamily: 'Gilroy',
               ),
             ),
-          ),
-          SizedBox(
-            height: size.height * 0.05,
-          ),
-          Container(
-            width: size.width * 0.8,
-            height: size.height * 0.07,
-            decoration: const BoxDecoration(
-              color: Color(0xff8871E6),
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-            ),
-            child: const Center(
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontFamily: 'Gilroy',
-                ),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 10),
-            width: size.width * 0.8,
-            height: size.height * 0.07,
-            decoration: BoxDecoration(
-              border: Border.all(color: const Color(0xff8871E6), width: 3),
-              color: Colors.white,
-              borderRadius: const BorderRadius.all(Radius.circular(15)),
-            ),
-            child: const Center(
-              child: Text(
-                'Register',
-                style: TextStyle(
-                  color: Color(0xff8871E6),
-                  fontSize: 20,
-                  fontFamily: 'Gilroy',
-                ),
-              ),
-            ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
