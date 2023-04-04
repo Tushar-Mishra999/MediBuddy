db3ae03f0296b78006af8a891361392e36bcb850
PH350 <75090103+PH350@users.noreply.github.com>
2023-04-04 10:50:46 +0530

    search api

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,3 +1,4 @@
+const { query, response } = require("express");
 const express = require("express");
 const Client = require("../models/client");
 const DailyTip = require("../models/dailytip");
@@ -20,7 +21,31 @@ clientRouter.get("/tipnsellers", async(req, res) => {
     const randomIndex = Math.floor(Math.random() * dailytipsArray.length);
     const randomTip = dailytipsArray[randomIndex];
 
-    res.json({"sellerList": sellerArray, "dailyTip": randomTip});
+-    res.json({"sellerList": sellerArray, "dailyTip": randomTip});
 });
 
+clientRouter.get("/search", async(req, res) => {
+    const city = req.body.city;
+    const medicine = req.query.medicine;
+    const sellers = await Seller.find({city});
+    let sellerArray = sellers.map((seller) => seller.toObject());
+    const searchedSellers= new Array;
+    for (let i= 0; i<sellerArray.length;i++)
+    {
+        let seller=sellerArray[i];
+        for (let j = 0; j < seller.stock.length;j++)
+        {
+            if(medicine == seller.stock[j])
+            {
+                searchedSellers.push(seller);
+            }
+            else
+            { 
+                continue;
+            }
+        }    
+    }
+    res.json({searchedSellers});
+});
 module.exports = clientRouter;
+
