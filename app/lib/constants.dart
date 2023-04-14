0a31e85c2e0ed24f41dd05c194c0333161e4bced
Tushar-Mishra999 <75738710+Tushar-Mishra999@users.noreply.github.com>
2023-04-14 17:26:24 +0530

    fixes

--- app/lib/constants.dart
+++ app/lib/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const color1 = Color(0xff8871E6);
 const color2 = Color(0xFFE7E8F1);
 const color3 = Color.fromARGB(255, 238, 238, 240);
-const ip = 'http://10.6.17.53:3000';
+const ip = 'https://whale-app-ud4yn.ondigitalocean.app';
 
 const description = {
   'Homeopathic':"Homeopathic medicine is based on the principle of 'like cures like,' using highly diluted substances to stimulate the body's natural healing processes. It considers the person's overall health, rather than just the symptoms of the disease.",
